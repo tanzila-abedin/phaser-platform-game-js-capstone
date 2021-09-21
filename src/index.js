13d8886cf82516ea70e229bebaa57fc21cb457e9
tanzila-abedin <tanzila.abedin@yahoo.com>
2021-09-21 14:36:33 +0200

    Commit changes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
-import _ from 'lodash'
 
-function component() {
-  const element = document.createElement("div");
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
+    var config = {
+        type: Phaser.AUTO,
+        width: 800,
+        height: 600,
+        scene: {
+            preload: preload,
+            create: create,
+            update: update
+        }
+    };
 
-  return element;
-}
+    var game = new Phaser.Game(config);
 
-document.body.appendChild(component());
+    function preload ()
+    {
+        this.load.image('sky', 'assets/sky.png');
+        this.load.image('ground', 'assets/platform.png');
+        this.load.image('star', 'assets/star.png');
+        this.load.image('bomb', 'assets/bomb.png');
+        this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 });
+    }
+
+    function create ()
+    {
+        this.add.image(400, 300, 'sky');
+    }
+
+    function update ()
+    {
+    }
